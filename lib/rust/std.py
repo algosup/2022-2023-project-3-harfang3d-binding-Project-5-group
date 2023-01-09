b121305bbb46977e959110e42567cd2ab5022450
Malo-Archimbaud <malo.archimbaud@algosup.com>
2023-01-09 16:30:11 +0100

    Update std.py
    Replace Go by Rust in function name

--- lib/rust/std.py
+++ lib/rust/std.py
@@ -35,7 +35,7 @@ def bind_std(gen):
             return f"{out_var} = C.GoString({in_var})\n"
         
     
-    class GoBasicTypeConverter(lang.rust.RustTypeConverterCommon):
+    class RustBasicTypeConverter(lang.rust.RustTypeConverterCommon):
         def __init__(self, type, c_type, rust_type, to_c_storage_type=None, bound_name=None, from_c_storage_type=None, needs_c_storage_class=False):
             super().__init__(type, to_c_storage_type, bound_name, from_c_storage_type, needs_c_storage_class)
             self.rust_to_c_type = c_type
