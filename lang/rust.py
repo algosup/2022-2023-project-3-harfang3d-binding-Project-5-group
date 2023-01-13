5efbf68ce622f579f688eb224ce9b17fdd5323bc
Paul Maris <paul.marisoumary@algosup.com>
2023-01-13 16:25:00 +0100

    add bunch of (useless but required) functions

--- lang/rust.py
+++ lang/rust.py
@@ -205,3 +205,42 @@ class RustGenerator(gen.FABGen):
     
     def start(self, module_name):
         super().start(module_name)
+
+    def set_error(self, type, reason):
+        return ""
+
+    def get_self(self, ctx):
+        return ""
+
+    def get_var(self, i, ctx):
+        return ""
+
+    def open_proxy(self, name, max_arg_count, ctx):
+        return ""
+
+    def _proto_call(self, self_conv, proto, expr_eval, ctx, fixed_arg_count=None):
+        return ""
+
+    def _bind_proxy(self, name, self_conv, protos, desc, expr_eval, ctx, fixed_arg_count=None):
+        return ""
+
+    def close_proxy(self, ctx):
+        return ""
+
+    def proxy_call_error(self, msg, ctx):
+        return ""
+
+    def return_void_from_c(self):
+        return ""
+
+    def rval_from_nullptr(self, out_var):
+        return ""
+
+    def rval_from_c_ptr(self, conv, out_var, expr, ownership):
+        return ""
+
+    def commit_from_c_vars(self, rvals, ctx="default"):
+        return ""
+
+    def rbind_function(self, name, rval, args, internal=False):
+        return ""
